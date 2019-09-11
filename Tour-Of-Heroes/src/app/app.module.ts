b8d3e99f2daa0499ba8fbd1d519664dd9089e645
Goofy5752 <m.asenov5752@gmail.com>
2019-09-11 18:06:25 +0300

    sidebar component added

--- Tour-Of-Heroes/src/app/app.module.ts
+++ Tour-Of-Heroes/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { FooterComponent } from './footer/footer.component';
     HeroSearchComponent,
     NavComponent,
     FooterComponent,
+    SidebarComponent,
   ],
   bootstrap: [AppComponent]
 })
