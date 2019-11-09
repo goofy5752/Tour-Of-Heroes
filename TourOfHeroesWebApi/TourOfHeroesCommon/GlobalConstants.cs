3c8cbe34acc35a50e01fcedc801e3e404e8cc3f5
Martin Asenov <m.asenov5752@gmail.com>
2019-11-09 19:59:57 +0200

    removed unused const AdminOrEditor

--- TourOfHeroesWebApi/TourOfHeroesCommon/GlobalConstants.cs
+++ TourOfHeroesWebApi/TourOfHeroesCommon/GlobalConstants.cs
@@ -6,6 +6,5 @@
         public static readonly string UserRole = "User";
         public static readonly string EditorRole = "Editor";
         public static readonly string EditorRoleCode = "qwerty123";
-        public static readonly string AdminOrEditor = AdminRole + "," + EditorRole;
     }
 }
