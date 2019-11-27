abdb6be29118cdb0aef8be1625f5e521e7cc5a98
Martin Asenov <m.asenov5752@gmail.com>
2019-11-27 18:40:25 +0200

    moved the logic for likedmovies in new controller

---
+++ TourOfHeroesWebApi/TourOfHeroesWebApi/Controllers/LikedMoviesController.cs
@@ -0,0 +1,89 @@
+﻿namespace TourOfHeroesWebApi.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using TourOfHeroesData.Models;
+    using TourOfHeroesServices.Contracts;
+    using Microsoft.AspNetCore.Authorization;
+    using TourOfHeroesDTOs.HeroDtos;
+    using TourOfHeroesDTOs.MovieDtos;
+
+    [Authorize]
+    public class LikedMoviesController : ApiController
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMovieService _movieService;
+
+        public LikedMoviesController(ILoggerManager logger, IMovieService movieService)
+        {
+            _logger = logger;
+            _movieService = movieService;
+        }
+
+        #region GetLikedMovies
+
+        [HttpGet("{likes}")]
+        [DisableRequestSizeLimit]
+        [Route("likedmovies/{likes}")]
+        public PageResultDTO<GetLikedMovieDTO> GetLikedMovies(int? page, int pageSize = 12)
+        {
+            _logger.LogInfo("Fetching all the liked movies from the storage...");
+
+            var userId = HttpContext.User.Claims.First(x => x.Type == "UserID").Value;
+
+            var countDetails = this._movieService.GetLikedMovies(userId).Count();
+            var result = new PageResultDTO<GetLikedMovieDTO>
+            {
+                Count = countDetails,
+                PageIndex = page ?? 1,
+                PageSize = pageSize,
+                Items = this._movieService.GetLikedMovies(userId).Skip((page - 1 ?? 0) * pageSize).Take(pageSize).ToList()
+            };
+
+            _logger.LogInfo($"Returning {countDetails} movies.");
+
+            return result;
+        }
+
+        #endregion
+
+        #region LikeMovie
+
+        [HttpPost("{like}")]
+        [DisableRequestSizeLimit]
+        [Route("likedmovies/{like}")]
+        public async Task<ActionResult<AddToLikesMovieDTO>> LikeMovie([FromForm] AddToLikesMovieDTO movie)
+        {
+            _logger.LogInfo($"Adding movie with title {movie.Title} to liked...");
+
+            await this._movieService.LikeMovie(movie);
+
+            _logger.LogInfo($"Successfully liked movie with title {movie.Title}...");
+
+            return this.Ok();
+        }
+
+        #endregion
+
+        #region DislikeMovie
+
+        [HttpDelete("{dislike}")]
+        [DisableRequestSizeLimit]
+        [Route("likedmovies/{dislike}")]
+        public async Task<ActionResult<LikedMovie>> DislikeMovie(string movieId)
+        {
+            var userId = HttpContext.User.Claims.First(x => x.Type == "UserID").Value;
+
+            _logger.LogInfo($"Disliking movie with id {movieId}...");
+
+            await this._movieService.DislikeMovie(userId, int.Parse(movieId));
+
+            _logger.LogInfo($"Successfully disliked movie with id {movieId}...");
+
+            return this.NoContent();
+        }
+
+        #endregion
+    }
+}
--- TourOfHeroesWebApi/TourOfHeroesWebApi/Controllers/MoviesController.cs
+++ TourOfHeroesWebApi/TourOfHeroesWebApi/Controllers/MoviesController.cs
@@ -7,8 +7,6 @@
     using TourOfHeroesData.Models;
     using TourOfHeroesServices.Contracts;
     using Validator.Contracts;
-    using TourOfHeroesDTOs.MovieDtos;
-    using TourOfHeroesDTOs.HeroDtos;
 
     [Authorize]
     public class MoviesController : ApiController
@@ -24,57 +22,12 @@
             _userValidator = userValidator;
         }
 
-        #region GetLikedMovies
-
-        [HttpGet("{likes}")]
-        [DisableRequestSizeLimit]
-        [Route("movies/{likes}")]
-        public PageResultDTO<GetLikedMovieDTO> GetLikedMovies(int? page, int pageSize = 12)
-        {
-            _logger.LogInfo("Fetching all the liked movies from the storage...");
-
-            var userId = HttpContext.User.Claims.First(x => x.Type == "UserID").Value;
-
-            var countDetails = this._movieService.GetLikedMovies(userId).Count();
-            var result = new PageResultDTO<GetLikedMovieDTO>
-            {
-                Count = countDetails,
-                PageIndex = page ?? 1,
-                PageSize = pageSize,
-                Items = this._movieService.GetLikedMovies(userId).Skip((page - 1 ?? 0) * pageSize).Take(pageSize).ToList()
-            };
-
-            _logger.LogInfo($"Returning {countDetails} movies.");
-
-            return result;
-        }
-
-        #endregion
-
-        #region LikeMovie
-
-        [HttpPost("{like}")]
-        [DisableRequestSizeLimit]
-        [Route("movies/{like}")]
-        public async Task<ActionResult<AddToLikesMovieDTO>> LikeMovie([FromForm] AddToLikesMovieDTO movie)
-        {
-            _logger.LogInfo($"Adding movie with title {movie.Title} to liked...");
-
-            await this._movieService.LikeMovie(movie);
-
-            _logger.LogInfo($"Successfully liked movie with title {movie.Title}...");
-
-            return this.Ok();
-        }
-
-        #endregion
-
         #region DeleteMovie
 
         [HttpDelete("{title}")]
         [DisableRequestSizeLimit]
         [Route("movies/{title}")]
-        public async Task<ActionResult<Hero>> DeleteMovie(string title, string password)
+        public async Task<ActionResult<Movie>> DeleteMovie(string title, string password)
         {
             var userId = HttpContext.User.Claims.First(x => x.Type == "UserID").Value;
 
@@ -86,25 +39,6 @@
             await this._movieService.DeleteMovie(title);
 
             _logger.LogInfo($"Successfully deleted movie with id {title}...");
-
-            return this.NoContent();
-        }
-
-        #endregion
-
-        //TODO: IMPLEMENT THE RIGHT LOGIC FLOW FOR DISLIKE MOVIE BUTTON
-        #region DislikeMovie
-
-        [HttpDelete("{title}")]
-        [DisableRequestSizeLimit]
-        [Route("movies/{title}")]
-        public async Task<ActionResult<Hero>> DislikeMovie(int movieId)
-        {
-            _logger.LogInfo($"Deleting movie with id {movieId}...");
-
-            await this._movieService.DislikeMovie(movieId);
-
-            _logger.LogInfo($"Successfully deleted movie with id {movieId}...");
 
             return this.NoContent();
         }
