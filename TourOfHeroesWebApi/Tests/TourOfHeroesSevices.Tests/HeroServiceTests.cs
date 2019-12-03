36c6c6af680fd21df6b55739abc5d2b0c61003cc
Martin Asenov <m.asenov5752@gmail.com>
2019-12-04 01:56:39 +0200

    configure xunit to work properly

---
+++ TourOfHeroesWebApi/Tests/TourOfHeroesSevices.Tests/HeroServiceTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TourOfHeroesServices.Tests
+{
+    using Xunit;
+    using System;
+
+    public class HeroServiceTests
+    {
+        [Fact]
+        public void ShouldReturnTrue()
+        {
+            var result = Math.Pow(2, 10);
+            var expected = 1024;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
