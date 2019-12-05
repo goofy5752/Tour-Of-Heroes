cf21fccaec3200250caea6700e662d17f44f3d6f
Martin Asenov <m.asenov5752@gmail.com>
2019-12-05 16:53:00 +0200

    added test for UserService

---
+++ TourOfHeroesWebApi/Tests/TourOfHeroesSevices.Tests/UserServiceTests.cs
@@ -0,0 +1,45 @@
+﻿namespace TourOfHeroesServices.Tests
+{
+    using Moq;
+    using Xunit;
+    using Contracts;
+    using System.Linq;
+    using System.Collections.Generic;
+    using TourOfHeroesData.Models;
+    using TourOfHeroesData.Common.Contracts;
+
+    public class UserServiceTests
+    {
+        public List<ApplicationUser> GetTestData()
+        {
+            return new List<ApplicationUser>
+            {
+                new ApplicationUser {
+                    Id = "1",
+                    UserName = "Pesho",
+                    Email = "test@gmail.com",
+                    FullName = "Martin Asenov"
+                },
+                new ApplicationUser {
+                    Id = "2",
+                    UserName = "Gosho",
+                    Email = "test1@gmail.com",
+                    FullName = "Gosho Goshev"
+                },
+            };
+        }
+
+        [Fact]
+        public void RepositoryShouldCallAllMethodOnce()
+        {
+            var repo = new Mock<IRepository<ApplicationUser>>();
+
+            repo.Setup(r => r.All())
+                .Returns(GetTestData().AsQueryable());
+
+            IUserService service = new UserService(repo.Object, null);
+
+            repo.Verify(x => x.All(), Times.Once);
+        }
+    }
+}
