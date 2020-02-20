4b0dd9f4b1af275e006af44d89ada7fe69efeda8
Martin Asenov <m.asenov5752@gmail.com>
2020-02-20 17:57:36 +0200

    set up InMemoryDb to work properly

---
+++ TourOfHeroesWebApi/Tests/TourOfHeroesSevices.Tests/Common/TourOfHeroesDbContextInMemoryFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TourOfHeroesServices.Tests.Common
+{
+    using System;
+
+    using TourOfHeroesData;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public static class TourOfHeroesDbContextInMemoryFactory
+    {
+        public static TourOfHeroesDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<TourOfHeroesDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new TourOfHeroesDbContext(options);
+        }
+    }
+}
