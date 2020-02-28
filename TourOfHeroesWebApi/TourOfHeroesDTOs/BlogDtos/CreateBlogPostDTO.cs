f7c4dbec38d94a391348edaa5b4adc77e6d667a7
Martin Asenov <m.asenov5752@gmail.com>
2020-02-28 23:03:40 +0200

    property attributes validations added

--- TourOfHeroesWebApi/TourOfHeroesDTOs/BlogDtos/CreateBlogPostDTO.cs
+++ TourOfHeroesWebApi/TourOfHeroesDTOs/BlogDtos/CreateBlogPostDTO.cs
@@ -1,13 +1,19 @@
 ﻿namespace TourOfHeroesDTOs.BlogDtos
 {
+    using System.ComponentModel.DataAnnotations;
+
     using Microsoft.AspNetCore.Http;
 
     public class CreateBlogPostDTO
     {
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(50)]
         public string Content { get; set; }
 
+        [Required]
         public IFormFile BlogImage { get; set; }
     }
 }
